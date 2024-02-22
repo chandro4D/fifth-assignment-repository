caab43dcba757928e060315eaf4232b471e87936
chandro4D <chandroshakhor7@gmail.com>
2024-02-22 11:48:23 +0600

    add button color

--- javascript/java.js
+++ javascript/java.js
@@ -1,12 +1,46 @@
-//function colorChange(){
-// document.getElementById('btn').style.backgroundColor='#1DD100';
-//}
+function setInnerText(id,value){
+  document.getElementById(id).innerText =value;
+}
+  //------------color change-------------
+function colorChange(){
+document.getElementById('btn').style.backgroundColor='#1DD100';
+}
+//--------------seat reduce-------------------
 const allBtn=document.getElementsByClassName("add-btn");
-let count =16;
+let count =32;
 for(const btn of allBtn){
-btn.addEventListener("click",function(){
+btn.addEventListener("click",function(e){
   count=count-1;
+  console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[5].innerText)
   document.getElementById("card-count").innerText = count;
-  document.getElementById('addBtn').style.backgroundColor='#1DD100';
+  
+ // document.getElementById('addBtn').style.backgroundColor='#1DD100';
 });
 }
+//----------------------color change--------------------
+function ColorChange(){
+  document.getElementById('addBtn').style.backgroundColor='#1DD100';
+  }
+
+function change(){
+    document.getElementById('change').style.backgroundColor='#1DD100';
+    }
+function Change(){
+      document.getElementById('Change').style.backgroundColor='#1DD100';
+      }
+function color(){
+        document.getElementById('color').style.backgroundColor='#1DD100';
+        }
+function Color(){
+          document.getElementById('Color').style.backgroundColor='#1DD100';
+          }
+function COlor(){
+            document.getElementById('COlor').style.backgroundColor='#1DD100';
+            }
+function COLOR(){
+              document.getElementById('COLOR').style.backgroundColor='#1DD100';
+              }          
+const All=document.getElementById("btn").innerText;
+console.log(All);   
+  
+      
